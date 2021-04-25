0b4bc2ef1932cac7638094848dd0be05a8bee631
Denyson Grellert <denysonjurgen@gmail.com>
2021-04-25 12:13:16 -0300

    Add comments about functions and their arguments/return

--- common/graph.py
+++ common/graph.py
@@ -4,24 +4,44 @@ Author: Denyson Grellert
 
 from igraph import *
 
+"""
+Makes a graph from a dictionary with all connections between the nodes
+    
+    Args:
+        connections 	-> A dictionary with the nodes and the connections
+                          between them
+
+    Return:
+        g -> graph with all nodes in connections and edges where exist a connection
+"""
 def connections2igraph(connections):
-	g = Graph()
+    g = Graph()
 
-	g.add_vertices(len(connections))
-	g.vs["name"] = list(connections.keys())
-	g.vs["label"] = g.vs["name"]
+    g.add_vertices(len(connections))
+    g.vs["name"] = list(connections.keys())
+    g.vs["label"] = g.vs["name"]
 
-	for a in connections:
-		for b in connections[a]:
-			g.add_edges([(a,b)])
-			g[a,b] = connections[a][b]
+    for a in connections:
+        for b in connections[a]:
+            g.add_edges([(a,b)])
+            g[a,b] = connections[a][b]
 
-	return g
+    return g
 
+
+"""
+Receive a graph and return a graph without separeted vertices
+
+    Args:
+        graph 	-> A graph
+
+    Return:
+        graph -> graph without separeted vertices
+"""
 def delete_separeted_vertices(graph):
 
-	degrees = graph.degree()
+    degrees = graph.degree()
 
-	remove = [i for i in range(len(degrees)) if degrees[i] == 0]
+    remove = [i for i in range(len(degrees)) if degrees[i] == 0]
 
-	graph.delete_vertices(remove)
+    graph.delete_vertices(remove)
