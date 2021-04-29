c254f82de402ee35f6229e645998a5e2054aa502
Denyson Grellert <denysonjurgen@gmail.com>
2021-04-29 10:14:04 -0300

    Change tab for spaces

--- common/convert.py
+++ common/convert.py
@@ -6,14 +6,14 @@ import pdfminer.high_level
 from unicodedata import normalize
 
 def pdf2string(file, pages=None):
-	return normalize("NFKD", pdfminer.high_level.extract_text(file, page_numbers=pages))
+    return normalize("NFKD", pdfminer.high_level.extract_text(file, page_numbers=pages))
 
 def pdf2txt(file, name, pages=None):
-	text = pdf2string(file, pages)
-	with open(name, 'w') as f:
-		f.write(text)
+    text = pdf2string(file, pages)
+    with open(name, 'w') as f:
+        f.write(text)
 
 def txt2string(file):
-	with open(file, 'r') as f:
-		string = f.read()
-	return string
+    with open(file, 'r') as f:
+        string = f.read()
+    return string
